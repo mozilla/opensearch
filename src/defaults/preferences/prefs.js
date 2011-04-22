16a28aa70b84b1334312bf3e865a9dab232313c2
Blake Winton <bwinton@latte.ca>
2011-04-21 21:08:22 -0400

    Order the search plugins in a locale-ish way.

---
+++ src/defaults/preferences/prefs.js
@@ -0,0 +1,17 @@
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.opensearch@mozillamessaging.com.description", "chrome://getanaccount/locale/opensearch.properties");
+
+// pointer to the default engine name
+pref("browser.search.defaultenginename", "chrome://opensearch/locale/opensearch.properties");
+
+// Ordering of Search Engines in the Engine list.
+pref("browser.search.order.1", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.2", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.3", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.4", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.5", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.6", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.7", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.8", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.9", "chrome://opensearch/locale/opensearch.properties");
+pref("browser.search.order.10", "chrome://opensearch/locale/opensearch.properties");
